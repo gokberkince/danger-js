4c756a894236f88e2d6b357108e0366d6404b15e
gokberk.ince <gokberk.ince@Gokberks-MacBook-Pro.local>
2023-12-21 18:23:24 +0300

    Revert "upped version"
    This reverts commit 1fc717af5f6e816ea09e1c5c5cec57d30f706154.

--- distribution/platforms/gitlab/GitLabGit.d.ts
+++
@@ -1,6 +0,0 @@
-import { GitLabDSL } from "../../dsl/GitLabDSL";
-import { GitDSL, GitJSONDSL } from "../../dsl/GitDSL";
-import { Types } from "@gitbeaker/node";
-import GitLabAPI from "./GitLabAPI";
-export declare const gitLabGitDSL: (gitlab: GitLabDSL, json: GitJSONDSL, gitlabAPI: GitLabAPI) => GitDSL;
-export declare const gitlabChangesToDiff: (changes: Types.CommitDiffSchema[]) => string;
